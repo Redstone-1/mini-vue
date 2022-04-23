b563ea49d02bc92e95ffd8a21fdab45b9f2c41af
redstone <xiwenge@come-future.com>
2022-04-23 20:11:05 +0800

    feat: 增加标签开头匹配

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state";
+import { compilerToFunction } from './compiler/index';
 
 export function initMixin (Vue) {
   Vue.prototype._init = function (options) {
@@ -6,5 +7,25 @@ export function initMixin (Vue) {
     vm.$options = options;
     // 初始化状态
     initState(vm)
+    // 挂载模板
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el);
+    }
+  }
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const options = vm.$options
+    el = document.querySelector(el);
+
+    if (!options.render) {
+      let template = options.template;
+      if (!template && el) {
+        template = el.outerHTML;
+        console.log('template>>>>>', template);
+        const render = compilerToFunction(template);
+        options.render = render;
+      }
+    }
   }
 }
